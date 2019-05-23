181787b27929e0e3e9c78434305cc1553f12d9da
Vlad Zhulinskiy <x-outhink@mail.ru>
2019-05-23 23:53:47 +0300

    исправления header

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-menu.onclick = function myFunction() {
-    var x = document.getElementById('myTopnav')
-    
-    if (x.className === "topnav") {
-        x.className += "responsive";
-    } else {
-        x.className = "topnav";
-    }
+// константы (ссылки на элементы DOM)
+const menu = document.getElementById('menu')
+const topnav = document.getElementById('myTopnav')
+// вешаем листенер на элемент DOM
+menu.addEventListener('click', toogleMenu)
+// функкция переключения активности меню
+function toogleMenu() {
+  topnav.classList.toggle('responsive')
 }
